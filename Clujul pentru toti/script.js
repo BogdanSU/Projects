7b8af3695b69c02397d6a641d769d825c8466851
BogdanSU <38920012+BogdanSU@users.noreply.github.com>
2019-07-07 19:26:23 +0300

    Add files via upload

---
+++ Clujul pentru toti/script.js
@@ -0,0 +1,13 @@
+function sad (){
+document.getElementById('non').src='./Imagini/sad.jpg';
+}
+
+function neutral (){
+document.getElementById('non').src='./Imagini/neutral.jpg';
+}
+
+function happy (){
+document.getElementById('non').src='./Imagini/happy.png';
+}
+
+
